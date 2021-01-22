e059547c7a0fe8a989d03450499a897f60ccc9ac
Sarah Sipple <hello@sarahsipple.com>
2021-01-22 15:33:23 -0500

    updated to connect to backend

--- src/App.js
+++ src/App.js
@@ -33,14 +33,25 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
 
-componentDidMount() {
-  fetch('http://localhost:3001')
-  .then(response => response.json())
-  .then(console.log)
+loadUser = (data) => {
+  this.setState({user: {
+    id: data.id,
+    name: data.name,
+    email: data.email,
+    entries: data.entries,
+    joined: data.joined
+  }})
 }
 
 calculateFaceLocation = (data) => {
@@ -70,8 +81,25 @@ onButtonSubmit = () => {
     .predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-  .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-  .catch(error => console.log(error));  
+    .then(response => {
+      console.log('hi', response)
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count}))
+          })
+
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err));
 }
 
 onRouteChange = (route) => {
@@ -101,7 +129,7 @@ render() {
          : (
            route === "signin"
            ? <Signin onRouteChange={this.onRouteChange} />
-           : <Register onRouteChange={this.onRouteChange} />
+           : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
          )
        }
     </div>
