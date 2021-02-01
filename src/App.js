68809c425a5903fc09172691b96cac28d4720da2
Sarah Sipple <hello@sarahsipple.com>
2021-02-01 15:24:59 -0500

    updated with serve package

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ onButtonSubmit = () => {
   .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('https://frozen-ravine-64980.herokuapp.com/:3000/image', {
+        fetch('https://frozen-ravine-64980.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
